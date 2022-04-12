1ec457974c8f7022c5197eb528273d3edf2e10a1
John Watson <42574977+johnwatson484@users.noreply.github.com>
2022-04-12 16:17:02 +0100

    Close connection on send (#3)

--- app/messaging/publish-event-request.js
+++ app/messaging/publish-event-request.js
@@ -1,13 +1,14 @@
 const { MessageSender } = require('ffc-messaging')
 const createMessage = require('./create-message')
 
-const publishEventRequest = async (eventMessage, config, type) => {
+const publishEventRequest = async (eventMessage, config) => {
   const eventSender = new MessageSender(config)
   const messageType = eventMessage.properties.action.type
   const source = eventMessage.properties.checkpoint
   eventMessage.properties.action.timestamp = new Date().toISOString()
   const message = createMessage(eventMessage, messageType, source)
   await eventSender.sendMessage(message)
+  await eventSender.closeConnection()
 }
 
 module.exports = publishEventRequest
