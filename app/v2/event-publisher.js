c39eb1c3ca45c15ad37b271eb473703269734cff
John Watson <johnwatson484@hotmail.com>
2023-02-18 20:24:24 +0000

    Rename v

--- app/v2/event-publisher.js
+++ app/v2/event-publisher.js
@@ -13,7 +13,11 @@ class EventPublisher {
   }
 
   async publishEvents (events) {
+    console.log('Received events:')
+    console.log(events)
     const validEvents = events.filter(validateEvent)
+    console.log('Valid events:')
+    console.log(validEvents)
     await sendMessageBatch(validEvents, this.config)
   }
 }
